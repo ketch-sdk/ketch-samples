3e423e4ad9f864c97534f6a3e10b3082498f73de
Justin Boileau <justin.boileau@ketch.com>
2024-02-29 22:55:15 -0800

    Added example listener

--- ketch-ios/iOS Ketch SDK SwiftUI/KetchSDK/ContentView.swift
+++ ketch-ios/iOS Ketch SDK SwiftUI/KetchSDK/ContentView.swift
@@ -6,10 +6,65 @@
 import SwiftUI
 import KetchSDK
 
+// Example custom listener that simply logs all events
+class MyKetchEventListener: KetchEventListener {
+    func onLoad() {
+        print("UI Loaded")
+    }
+
+    func onShow() {
+        print("UI Shown")
+    }
+
+    func onDismiss() {
+        print("UI Dismissed")
+    }
+
+    func onEnvironmentUpdated(environment: String?) {
+        print("Environment Updated: \(String(describing: environment))")
+    }
+
+    func onRegionInfoUpdated(regionInfo: String?) {
+        print("Region Info Updated: \(String(describing: regionInfo))")
+    }
+
+    func onJurisdictionUpdated(jurisdiction: String?) {
+        print("Jurisdiction Updated: \(String(describing: jurisdiction))")
+    }
+
+    func onIdentitiesUpdated(identities: String?) {
+        print("Identities Updated: \(String(describing: identities))")
+    }
+
+    func onConsentUpdated(consent: KetchSDK.ConsentStatus) {
+        print("Consent Updated: \(consent)")
+    }
+
+    func onError(description: String) {
+        print("Error: \(description)")
+    }
+
+    func onCCPAUpdated(ccpaString: String?) {
+        print("CCPA String Updated: \(String(describing: ccpaString))")
+    }
+
+    func onTCFUpdated(tcfString: String?) {
+        print("TCF String Updated: \(String(describing: tcfString))")
+    }
+
+    func onGPPUpdated(gppString: String?) {
+        print("GPP String Updated: \(String(describing: gppString))")
+    }
+}
+
 struct ContentView: View {
     @ObservedObject var ketchUI: KetchUI
     
+    // Define listener as a property of ContentView
+    let listener = MyKetchEventListener()
+    
     init() {
+        // Create the KetchSDK object
         let ketch = KetchSDK.create(
             organizationCode: "ketch_samples",
             propertyCode: "ios",
@@ -19,12 +74,16 @@ struct ContentView: View {
             ]
         )
         
+        // Create the KetchUI object
         ketchUI = KetchUI(
             ketch: ketch,
             experienceOptions: [
                 .forceExperience(.consent)
             ]
         )
+        
+        // Add our listener to the ketchUI class
+        ketchUI.eventListener = listener
     }
     
     @State var selectedExperienceToShow: KetchUI.ExperienceOption.ExperienceToShow = .consent
