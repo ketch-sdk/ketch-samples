e8c081b2d4cef7c9dd4d1cd6955f02f989df3108
Roman Simenok <roma.simenok@gmail.com>
2025-02-18 19:56:29 +0200

    New UI for iOS SwiftUI sample;

--- ketch-ios/sdk/ketch-ios-sample/KetchSDK/ContentView.swift
+++ ketch-ios/sdk/ketch-ios-sample/KetchSDK/ContentView.swift
@@ -6,62 +6,11 @@
 import SwiftUI
 import KetchSDK
 
-// Example custom listener that logs all events
-class MyKetchEventListener: KetchEventListener {
-    func onLoad() {
-        print("UI Loaded")
-    }
-
-    func onShow() {
-        print("UI Shown")
-    }
-
-    func onDismiss(status: KetchSDK.HideExperienceStatus) {
-        print("UI Dismissed, Status: \(status)")
-    }
-
-    func onEnvironmentUpdated(environment: String?) {
-        print("Environment Updated: \(String(describing: environment))")
-    }
-
-    func onRegionInfoUpdated(regionInfo: String?) {
-        print("Region Info Updated: \(String(describing: regionInfo))")
-    }
-
-    func onJurisdictionUpdated(jurisdiction: String?) {
-        print("Jurisdiction Updated: \(String(describing: jurisdiction))")
-    }
-
-    func onIdentitiesUpdated(identities: String?) {
-        print("Identities Updated: \(String(describing: identities))")
-    }
-
-    func onConsentUpdated(consent: KetchSDK.ConsentStatus) {
-        print("Consent Updated: \(consent)")
-    }
-
-    func onError(description: String) {
-        print("Error: \(description)")
-    }
-
-    func onCCPAUpdated(ccpaString: String?) {
-        print("CCPA String Updated: \(String(describing: ccpaString))")
-    }
-
-    func onTCFUpdated(tcfString: String?) {
-        print("TCF String Updated: \(String(describing: tcfString))")
-    }
-
-    func onGPPUpdated(gppString: String?) {
-        print("GPP String Updated: \(String(describing: gppString))")
-    }
-}
-
 struct ContentView: View {
-    @ObservedObject var ketchUI: KetchUI
+    @StateObject var ketchUI: KetchUI
     
     // Define listener as a property of ContentView
-    let listener = MyKetchEventListener()
+    let listener = SampleEventListener()
     
     init() {
         // Create the KetchSDK object
@@ -78,136 +27,194 @@ struct ContentView: View {
         )
         
         // Create the KetchUI object
-        ketchUI = KetchUI(
-            ketch: ketch
-        )
+        _ketchUI = StateObject(wrappedValue: KetchUI(ketch: ketch))
         
         // Add our listener to the ketchUI class
         ketchUI.eventListener = listener
     }
     
-    @State var selectedExperienceToShow: KetchUI.ExperienceOption.ExperienceToShow = .consent
-    @State var selectedTab: KetchUI.ExperienceOption.PreferencesTab?
-    @State var lang = "EN"
-    @State var jurisdiction = "default"
-    @State var region = "US"
-    @State var tabsExpanded = false
-    @State var selectedTabs = KetchUI.ExperienceOption.PreferencesTab.allCases
-    
-    @ViewBuilder
-    private func checkbox(_ value: Binding<Bool>) -> some View {
-        Button {
-            value.wrappedValue.toggle()
-        } label: {
-            Image(systemName: value.wrappedValue ? "circle.fill" : "circle")
-        }
-    }
+    @State private var selectedTabs: Set<KetchUI.ExperienceOption.PreferencesTab> = Set([.overviewTab, .consentsTab, .subscriptionsTab, .rightsTab])
+    @State private var selectedTab = KetchUI.ExperienceOption.PreferencesTab.overviewTab
+    @State private var apiRegion = APIRegion.us
+    @State private var org = ""
+    @State private var property = ""
+    @State private var env = ""
+    @State private var lang = ""
+    @State private var jurisdiction = ""
+    @State private var region = ""
+    @State private var idName = ""
+    @State private var idValue = ""
+    @State private var identities = [Ketch.Identity]()
     
     var body: some View {
-        ScrollView {
-            ZStack(alignment: .bottom) {
-                VStack(alignment: .leading) {
-                    Text("Experience:")
-                    Picker("Experience", selection: $selectedExperienceToShow) {
-                        ForEach([KetchUI.ExperienceOption.ExperienceToShow.consent, .preferences], id: \.self) {
-                            Text($0.name)
-                        }
-                    }
-                    .pickerStyle(.segmented)
-                    
-                    if selectedExperienceToShow == .preferences {
-                        Text("Tabs:")
-                        
-                        LazyVGrid(columns: [
-                            GridItem(.flexible()),
-                            GridItem(.flexible())
-                        ]) {
-                            ForEach(KetchUI.ExperienceOption.PreferencesTab.allCases, id: \.self) { tab in
-                                prefTabCheckMarkView(tab: tab)
-                            }
-                        }
-                        
-                        if !selectedTabs.isEmpty {
-                            HStack {
-                                Text("Active tab:")
-                                
-                                Picker("Active tab:", selection: $selectedTab) {
-                                    Text("none").tag(nil as KetchUI.ExperienceOption.PreferencesTab?)
-                                    
-                                    ForEach(selectedTabs, id: \.self) { tab in
-                                        Text(tab.rawValue.replacingOccurrences(of: "Tab", with: "")).tag(tab as KetchUI.ExperienceOption.PreferencesTab?)
-                                    }
-                                }
-                                .pickerStyle(.menu)
-                            }
-                        }
-                    }
-                    
-                    Text("Language:")
-                    Picker("Language", selection: $lang) {
-                        ForEach(["EN", "FR"], id: \.self) {
-                            Text($0)
-                        }
-                    }
-                    .pickerStyle(.segmented)
-                    
-                    Text("Jurisdiction:")
-                    Picker("Jurisdiction", selection: $jurisdiction) {
-                        ForEach(["default", "gdpr"], id: \.self) {
-                            Text($0)
-                        }
-                    }
-                    .pickerStyle(.segmented)
-                    
-                    
-                    Text("Region:")
-                    Picker("Region", selection: $region) {
-                        ForEach(["US", "FR", "GB"], id: \.self) {
-                            Text($0)
-                        }
-                    }
-                    .pickerStyle(.segmented)
-                    
+        VStack(alignment: .leading) {
+            Text("Global options")
+                .font(.title2)
+            Text("Options that apply to both experiences")
+                .font(.footnote)
+                .foregroundStyle(Color.gray)
+            
+            LazyVGrid(columns: [
+                GridItem(.flexible(), alignment: .center),
+                GridItem(.flexible(), alignment: .center),
+                GridItem(.flexible(), alignment: .center)
+            ]) {
+                
+                text("Organization", value: $org)
+                text("Property", value: $property)
+                text("Environment", value: $env)
+                
+                text("Language", value: $lang)
+                text("Jurisdiction", value: $jurisdiction)
+                text("Region", value: $region)
+                
+                text("Identities", value: $idName, prompt: "Name")
+                text(" ", value: $idValue, prompt: "Value")
+                
+                VStack {
                     Spacer()
+                        .frame(height: 18)
                     
-                    HStack {
+                    HStack(alignment: .center) {
+                        Button("Reset") {
+                            identities.removeAll()
+                        }
+                        .padding(.leading, 4)
+                        .disabled(identities.isEmpty)
+                        
                         Spacer()
                         
-                        Button("Show") {
-                            var params: [KetchUI.ExperienceOption?] = [
-                                .region(code: region),
-                                .language(code: lang),
-                                .forceExperience(selectedExperienceToShow),
-                                .jurisdiction(code: jurisdiction)
-                            ]
-                            
-                            if !selectedTabs.isEmpty && selectedExperienceToShow == .preferences {
-                                let selectedTabsNames = selectedTabs.compactMap { $0.rawValue }
-                                params.append(.preferencesTabs(selectedTabsNames.joined(separator: ",")))
-                                
-                                if let selectedTab, selectedTabs.contains(selectedTab) {
-                                    params.append(.preferencesTab(selectedTab))
-                                }
+                        Button("Add") {
+                            guard !idName.isEmpty, !idValue.isEmpty else {
+                                return
                             }
                             
-                            ketchUI.reload(with: params.compactMap{$0})
+                            identities.append(Ketch.Identity(key: idName, value: idValue))
                         }
-                        .font(.system(.title))
-                        
-                        Spacer()
-                    }
-                    
-                    Spacer()
-                    
-                    Button("Log local privacy strings") {
-                        showPrivacyStrings()
+                        .padding(.trailing, 4)
+                        .disabled(idName.isEmpty || idValue.isEmpty)
                     }
                 }
-                .padding()
             }
+            .padding(.top, 8)
+            .padding(.bottom, 16)
+            
+            Text("API Region")
+                .font(.subheadline)
+            
+            HStack {
+                apiButton(api: .us)
+                apiButton(api: .eu)
+                apiButton(api: .uat)
+            }
+            .padding(.bottom, 16)
+            
+            Text("Preference Options")
+                .font(.title2)
+            
+            Text("Options that only apply to the preference experience")
+                .font(.footnote)
+                .foregroundStyle(Color.gray)
+                .padding(.bottom, 8)
+            
+            Text("Allowed Tabs")
+                .font(.subheadline)
+            HStack {
+                checkbox(tab: .overviewTab, title: "Overview")
+                checkbox(tab: .consentsTab, title: "Consent")
+                checkbox(tab: .subscriptionsTab, title: "Subscriptions")
+                checkbox(tab: .rightsTab, title: "Rights")
+            }
+            .padding(.bottom, 8)
+            
+            
+            Text("Initial Tabs")
+                .font(.subheadline)
+            HStack {
+                tabButton(tab: .overviewTab, title: "Overview")
+                tabButton(tab: .consentsTab, title: "Consent")
+                tabButton(tab: .subscriptionsTab, title: "Subscriptions")
+                tabButton(tab: .rightsTab, title: "Rights")
+            }
+            .padding(.bottom, 16)
+            
+            Text("Actions")
+                .font(.title2)
+            
+            Text("Trigger some SDK funcionality")
+                .font(.footnote)
+                .foregroundStyle(Color.gray)
+            
+            HStack {
+                Button("Consent") {
+                    ketchUI.reload(with: makeParameters)
+                }
+                
+                Spacer()
+                
+                Button("Preferences") {
+                    var parameters = makeParameters
+                    
+                    if !selectedTabs.isEmpty {
+                        let selectedTabsNames = selectedTabs.compactMap { $0.rawValue }
+                        parameters.append(.preferencesTabs(selectedTabsNames.joined(separator: ",")))
+                        
+                        if selectedTabs.contains(selectedTab) {
+                            parameters.append(.preferencesTab(selectedTab))
+                        }
+                    }
+                    
+                    ketchUI.reload(with: parameters)
+                }
+                
+                Spacer()
+                
+                Button("Privacy Strings") {
+                    showPrivacyStrings()
+                }
+            }
+            .padding(.vertical)
+            
+            Spacer()
         }
+        .padding()
         .background(.white)
         .ketchView(model: $ketchUI.webPresentationItem)
+    }
+    
+    private var makeParameters: [KetchUI.ExperienceOption] {
+        var parameters = [KetchUI.ExperienceOption]()
+        if !org.isEmpty {
+            parameters.append(.organizationCode(org))
+        }
+        
+        if !property.isEmpty {
+            parameters.append(.propertyCode(property))
+        }
+        
+        if !env.isEmpty {
+            parameters.append(.environment(env))
+        }
+        
+        if !lang.isEmpty {
+            parameters.append(.language(code: lang))
+        }
+        
+        if !jurisdiction.isEmpty {
+            parameters.append(.jurisdiction(code: jurisdiction))
+        }
+        
+        if !region.isEmpty {
+            parameters.append(.region(code: region))
+        }
+        
+        parameters.append(.ketchURL(apiRegion.urlString))
+        
+        identities.forEach { identity in
+            parameters.append(.identity(identity))
+        }
+        
+        return parameters
     }
     
     private func showPrivacyStrings() {
@@ -244,33 +251,92 @@ struct ContentView: View {
         }
         print("* ----- End privacy strings ---- *\n")
     }
-    
-    private func prefTabCheckMarkView(tab: KetchUI.ExperienceOption.PreferencesTab) -> some View {
-        HStack {
-            Image(systemName: selectedTabs.contains(tab) ? "checkmark.square" : "square")
-            Spacer()
-            Text(tab.rawValue.replacingOccurrences(of: "Tab", with: ""))
+}
+
+// MARK: - UI
+
+fileprivate extension ContentView {
+    func text(_ text: String, value: Binding<String>, prompt: String? = nil) -> some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text(text)
+                .font(.subheadline)
+            
+            TextField("", text: value, prompt: prompt == nil ? nil : Text(prompt!))
+                .padding(4)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(lineWidth: 1)
+                        .foregroundStyle(Color.gray)
+                }
         }
-        .padding(2)
-        .background(.gray.opacity(0.2))
-        .cornerRadius(3)
-        .onTapGesture {
-            if let index = selectedTabs.firstIndex(of: tab) {
-                selectedTabs.remove(at: index)
-            } else {
-                selectedTabs.append(tab)
+    }
+    
+    func checkbox(tab: KetchUI.ExperienceOption.PreferencesTab, title: String) -> some View {
+        HStack(spacing: 4) {
+            Button {
+                if selectedTabs.contains(tab) {
+                    selectedTabs.remove(tab)
+                } else {
+                    selectedTabs.insert(tab)
+                }
+            } label: {
+                Image(systemName: selectedTabs.contains(tab) ? "checkmark.square.fill" : "square")
             }
+            
+            Text(title)
+                .font(.footnote)
+        }
+    }
+    
+    func tabButton(tab: KetchUI.ExperienceOption.PreferencesTab, title: String) -> some View {
+        HStack(spacing: 4) {
+            Button {
+                selectedTab = tab
+            } label: {
+                Image(systemName: selectedTab == tab ? "circle.fill" : "circle")
+            }
+            
+            Text(title)
+                .font(.footnote)
+        }
+    }
+    
+    func apiButton(api: APIRegion) -> some View {
+        HStack(spacing: 4) {
+            Button {
+                apiRegion = api
+            } label: {
+                Image(systemName: apiRegion == api ? "circle.fill" : "circle")
+            }
+            
+            Text(api.name)
+                .font(.footnote)
         }
     }
 }
 
-extension KetchUI.ExperienceOption.ExperienceToShow {
+enum APIRegion {
+    case us, eu, uat
+    
     var name: String {
         switch self {
-        case .consent:
-            return "Consent"
-        case .preferences:
-            return "Preferences"
+        case .us:
+            return "Prod US"
+        case .eu:
+            return "Prod EU"
+        case .uat:
+            return "UAT"
+        }
+    }
+    
+    var urlString: String {
+        switch self {
+        case .us:
+            return "https://global.ketchcdn.com/web/v3"
+        case .eu:
+            return "https://eu.ketchcdn.com/web/v3"
+        case .uat:
+            return "https://dev.ketchcdn.com/web/v3"
         }
     }
 }
